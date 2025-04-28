161ec0b1dbbe49233e7da2be402f305fb362863f
NotRelevant <michel.gouzee@hotmail.com>
2025-04-28 11:50:28 +0200

    add metrics for prometheus

--- Barstation/Program.cs
+++ Barstation/Program.cs
@@ -1,6 +1,7 @@
 // File: Program.cs
 using Barstation.Models;
 using Barstation.Services;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,30 +14,84 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Barstation API is running!");
 
+var transactionCounter = Metrics.CreateCounter("transactions_total", "Number of transactions processed");
+var errorCounter = Metrics.CreateCounter("errors_total", "Number of errors catched");
+var securityCounter = Metrics.CreateCounter("security_operations", "Number of lock/unlock processed");
+
 app.MapPost("/addValue", (Wallet wallet, decimal amount, IWalletOperations walletOperations, ITransactionRepository transactionRepository) =>
 {
-    return walletOperations.AddValue(wallet, amount);
+	try
+	{
+		var result = walletOperations.AddValue(wallet, amount);
+        transactionCounter.Increment();
+
+		return result;
+	}
+	catch (global::System.Exception)
+	{
+		errorCounter.Increment();
+		throw;
+	}
 });
 
 app.MapPost("/subtractValue", (Wallet wallet, decimal amount, IWalletOperations walletOperations, ITransactionRepository transactionRepository) =>
 {
-    return walletOperations.SubtractValue(wallet, amount);
+	try
+	{
+		var result = walletOperations.SubtractValue(wallet, amount);
+		transactionCounter.Increment();
+		return result;
+	}
+	catch (global::System.Exception)
+	{
+		errorCounter.Increment();
+		throw;
+	}
 });
 
 
 app.MapPost("/createWallet", (Wallet wallet, IWalletOperations walletOperations) =>
 {
-    return walletOperations.Create(wallet);
+	try
+	{
+		transactionCounter.Increment();
+		return walletOperations.Create(wallet);
+	}
+	catch (global::System.Exception)
+	{
+		errorCounter.Increment();
+		throw;
+	}
 });
 
 app.MapPut("/blockWallet", (string username, IWalletOperations walletOperations) =>
 {
-    return walletOperations.BlockByOwner(username);
+	try
+	{
+		securityCounter.Increment();
+		return walletOperations.BlockByOwner(username);
+	}
+	catch (global::System.Exception)
+	{
+		errorCounter.Increment();
+		throw;
+	}
 });
 
 app.MapPut("/unlockWallet", (Wallet wallet, IWalletOperations walletOperations ) =>
 {
-    walletOperations.Unlock(wallet);
+	try
+	{
+		securityCounter.Increment();
+		walletOperations.Unlock(wallet);
+	}
+	catch (global::System.Exception)
+	{
+		errorCounter.Increment();
+		throw;
+	}
 });
 
+
+app.UseMetricServer(5000);
 app.Run();
